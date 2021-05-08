569dec13924b1f4711949c1f8ef0b84b0c488475
BAVARESCO-PC\Bavaresco <maopdtec@gmail.com>
2021-05-08 12:31:44 -0300

    Escrita em arquivo

---
+++ arquivo3.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<locale.h>
+#include<string.h>
+int main()
+{
+	setlocale(LC_ALL,"portuguese");
+	char  stringParaEscrever[50];
+	FILE * arq;
+	printf("Frase que você gostaria de inserir no texto:\n");
+	scanf("%s",stringParaEscrever);
+	arq=fopen("texto.txt","w");
+	if(arq==NULL)
+	fprintf(stdout,"ARQUIVO NULO!\n");
+	else
+	fprintf(arq,"%s",stringParaEscrever);
+// 	fwrite(stringParaEscrever,1,strlen(stringParaEscrever),arq);
+	fclose(arq);
+	return 0;
+}
