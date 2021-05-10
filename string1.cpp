fae4a712186bf2ce1afe2c7e745ac81adc41ac20
BAVARESCO-PC\Bavaresco <maopdtec@gmail.com>
2021-05-10 10:37:06 -0300

    strtok delimitadores

--- string1.cpp
+++ string1.cpp
@@ -6,7 +6,10 @@ int main()
 	setlocale(LC_ALL,"portuguese");
  	char  string1[50];
 	 printf("\n\n\nDigite uma String:");
- 	scanf("%[^;]s",string1);
+ 	scanf("%[^\n]",string1);
+ 	//com espaço
+ 	//scanf("%[^;]s ",string1);
+ 	//com delimitador
  	printf(" %s ",string1);
 	
 	
